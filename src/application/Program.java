93e3c680deee516108ab678e9163c961465787b3
João Pedro <joaopedrov159@hotmail.com>
2024-04-02 10:29:56 -0300

    using <?>

--- src/application/Program.java
+++ src/application/Program.java
@@ -14,10 +14,15 @@ public class Program {
 		shapes.add(new Rectangle(3.0, 2.0));
 		shapes.add(new Circle(2.0));
 		
+		List<Circle> circles = new ArrayList<>();
+		circles.add(new Circle(2.0));
+		circles.add(new Circle(3.0));
+		
 		System.out.println("Total area = " + totalArea(shapes));
+		
 	}
 	
-	public static double totalArea(List<Shape> list) {
+	public static double totalArea(List<? extends Shape> list) {
 		double sum = 0;
 		
 		for(Shape s : list) {
