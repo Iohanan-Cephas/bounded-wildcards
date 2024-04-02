8b5da3706645d5a8d054dda1c3af4d241b7fb9ab
João Pedro <joaopedrov159@hotmail.com>
2024-04-02 11:27:37 -0300

    Problem 2 and README

--- README.md
+++ README.md
@@ -1,3 +1,50 @@
 # Bounded Wildcards
-Here we have a program that is going to return the sum of the areas of a list of figures using generics types. 
 This program was an example to help me fixing the way to use the wildcard type in a simple way.
+
+### Problem 1
+Here we have a script that is going to return the sum of the areas of a list of figures using generics types.
+
+### Problem 2
+Now, a script that copies the elements of a list to another one who can be more generic than the first.
+
+Below, we have the diagram of the wrapper types to aid in understanding the script 'Problem (2)' and to help grasp the Get/Put Principle as well.
+|Wrapper types||
+|--|--|
+|Object
+|l|
+|l|
+|l|
+|~~~~~~~~~~~>|Character|
+|l|
+|l|
+|l|
+|~~~~~~~~~~~>|Boolean|
+|l|
+|l|
+|l|
+|~~~~~~~~~~~>|Number|
+||l|
+||l|
+||l|
+||~~~~~~~~~~~>Integer
+||l|
+||l|
+||l|
+||~~~~~~~~~~~>Byte
+||l|
+||l|
+||l|
+||~~~~~~~~~~~>Long
+||l|
+||l|
+||l|
+||~~~~~~~~~~~>Short
+||l|
+||l|
+||l|
+||~~~~~~~~~~~>Float
+||l|
+||l|
+||l|
+||~~~~~~~~~~~>Double
+||
--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,7 @@
 package application;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import entities.Circle;
@@ -9,6 +10,8 @@ import entities.Shape;
 
 public class Program {
 	public static void main(String[] args) {
+		
+		// Problem (1)
 		List<Shape> shapes = new ArrayList<>();
 		
 		shapes.add(new Rectangle(3.0, 2.0));
@@ -19,7 +22,61 @@ public class Program {
 		circles.add(new Circle(3.0));
 		
 		System.out.println("Total area = " + totalArea(shapes));
+		// End (1)
 		
+		//------------------------------------------------------
+		
+		// Problem (2)
+		
+			/*	Get/put principle - Covariance
+				Wrong way:
+				List<Integer> list = new ArrayList<>();
+				list.add(10);
+				list.add(5);
+				
+				List<? extends Number> list2 = list;
+				
+				Number x = list2.get(0);
+				
+				list2.add(20); -> compilation error
+				
+				GET - ok
+				PUT - error
+				(Posso obter, mas não posso inserir)
+			 */
+		
+			/*
+			 	Get/put principle - Contravariance
+			 	Wrong way:
+			 	
+			 	List<Object> list3 = new ArrayList<>();
+			 	list3.add("maria");
+			 	list3.add("alex");
+			 	
+			 	List<? super Number> list4 = list3; -> "list4" aceita tanto Number quanto Object (super tipo de Number)
+			 	
+			 	list4.add(10);
+			 	list4.add(3.14);
+			 	
+			 	Number x = list4.get(0); -> compilation error
+			 	
+			 	GET - error
+			 	PUT - ok
+			 	(Posso inserir, mas não posso obter)
+			 */
+		
+			List<Integer> ints = Arrays.asList(1, 2, 3, 4);
+			List<Double> dbls = Arrays.asList(3.14, 6.28);
+			List<Object> objs = new ArrayList<Object>();
+			
+			copy(ints, objs);
+			copy(dbls, objs);
+			
+			printList(ints);
+			printList(dbls);
+			printList(objs);
+			
+			//End (2)
 	}
 	
 	public static double totalArea(List<? extends Shape> list) {
@@ -30,5 +87,18 @@ public class Program {
 		}
 		
 		return sum;
+	}
+	
+	public static void copy(List<? extends Number> source, List<? super Number> target) {
+		for(Number number : source) {
+			target.add(number);
+		}
+	}
+	
+	public static void printList(List<?> list) {
+		for(Object obj : list) {
+			System.out.print(obj + " ");
+		}
+		System.out.println();
 	}
 }
